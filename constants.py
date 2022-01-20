1d3209339418311f5ba35c604ea1deba2069f8e2
mrmamongo <63308103+mrmamongo@users.noreply.github.com>
2022-01-20 12:54:46 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,5 +6,6 @@ search_xpath = "//*[@id=\"headerContainer\"]/div/header/div[2]/div/div[2]/a"
 search_panel_xpath = "//*[@id=\"search-component\"]/input"
 found_value_xpath = "//*[@id=\"search-container\"]/div[2]/div/div/div[2]/a"
 game_name_xpath = "//*[@id=\"page__wrap\"]/div[4]/div[1]/div/div/div[2]/div/div/div[1]/div/div/div/div/div[1]/div[1]/div[1]/div[2]/a[2]/span"
+game_second_name_xpath = "//*[@id=\"page__wrap\"]/div[4]/div[1]/div/div/div[2]/div/div/div[1]/div/div/div/div/div[1]/div[1]/div[1]/div[2]/a[3]/span"
 
-delay = 5
+delay = 5 # seconds
